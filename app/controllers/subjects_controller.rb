7ccb9fcfd198c3688e32d69adb030f215ee62610
Sameer Soin <sameersoin95@gmail.com>
2019-01-03 18:05:45 +0530

    Subjects create edit and delete added

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -11,11 +11,49 @@ class SubjectsController < ApplicationController
   end
 
   def new
+    @subject = Subject.new
+  end
+
+  def create
+    @subject = Subject.new(subject_params)
+
+    if @subject.save
+      flash[:notice] = "Subect created successfully."
+      redirect_to(:action => 'index')
+    else
+      render('new')
+    end
   end
 
   def edit
+    @subject = Subject.find(params[:id])
+  end
+
+  def update
+    @subject = Subject.find(params[:id])
+
+    if @subject.update_attributes(subject_params)
+      flash[:notice] = "Subect updated successfully."
+      redirect_to(:action => 'show', :id => @subject.id)
+    else
+      render('edit')
+    end
   end
 
   def delete
+    @subject = Subject.find(params[:id])
   end
+
+  def destroy
+    subject = Subject.find(params[:id]).destroy
+    flash[:notice] = "Subect '#{subject.name}' destroyed successfully."
+    redirect_to(:action => 'index')
+  end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name, :position, :visible)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   
   #get 'demo/index'
   root "demo#index"
-  match ':controller(/:action(/:id))', :via => :get
+  match ':controller(/:action(/:id))', :via => [:get, :post]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
